8dcbaef9411786e9707f9b89052f09153333f79d
rbonnamy <rbonnamy@diginamic.fr>
2023-10-04 14:57:40 +0200

    v1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,26 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		
+		byte a = 12;
+		short b = 25653;
+		int c = 65156165;
+		long d = 65465615616651561L;
+		
+		float pi = 3.1415f;
+		double poids = 1256.32;
+		
+		boolean val = false;
+		
+		char cc = '\u6544';
+		String randomString = "Le résultat de ce calcul est:\n1+5=6";
+		
+		System.out.println(cc);
+		System.out.println(randomString);
+		
+
+	}
+
+}
